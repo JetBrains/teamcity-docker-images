8cf14a954869af5fa5fd9f532fe792ac62c9c3de
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2020-05-06 15:40:04 +0300

    minor in check scripts

--- checks/agent-nanoserver.cmd
+++ checks/agent-nanoserver.cmd
@@ -1,1 +1,1 @@
-docker run -it -e SERVER_URL="http://localhost:8111" -v "%CD%\conf:C:/BuildAgent/conf" teamcity-agent:eap-latest-nanoserver-1809
+docker run -it -e SERVER_URL="http://localhost:8111" -v "%CD%\conf:C:/BuildAgent/conf" teamcity-agent:latest-nanoserver-1809
