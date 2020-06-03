a583edc3df28311fa016722c18821b9abb7c4e4b
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2020-06-03 15:45:34 +0300

    fix generate scripts

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 bash tool/build-tool.sh
 rm -rf "context/generated"
-bin/TeamCity.Docker generate -s configs -f "configs/common.config;configs/windows.config;configs/linux.config" -c context/generated -t generated -d .teamcity -b "TC2019_2_BuildDist:2019_2;TC_Trunk_BuildDist" -r PROJECT_EXT_2307
+bin/TeamCity.Docker generate -s configs -f "configs/common.config;configs/windows.config;configs/linux.config" -c context -t context/generated -d .teamcity -b "TC2019_2_BuildDist:2019_2;TC_Trunk_BuildDist" -r PROJECT_EXT_2307
