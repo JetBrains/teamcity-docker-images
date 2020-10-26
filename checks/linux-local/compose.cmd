ccc31c7eed2c805ce772282bab57f8c8f85c7a85
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2020-10-26 13:08:44 +0300

    Updare readme

--- checks/linux-local/compose.cmd
+++ checks/linux-local/compose.cmd
@@ -1,4 +1,1 @@
-docker pull teamcity-server:local-linux
-docker pull teamcity-minimal-agent:local-linux
-docker pull teamcity-agent:local-linux
 docker-compose up
--- checks/windows-local/compose.cmd
+++ checks/windows-local/compose.cmd
@@ -1,5 +1,1 @@
-docker pull teamcity-server:local-nanoserver-1903
-docker pull teamcity-minimal-agent:local-nanoserver-1903
-docker pull teamcity-agent:local-nanoserver-1903
-docker pull teamcity-agent:local-windowsservercore-1903
 docker-compose up
