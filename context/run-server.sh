579b54553ef24dcf6d71c6792acbe028a3c2105c
Kirill (KIR) Maximov <kir@maxkir.com>
2020-12-09 20:20:52 +0100

    Use TEAMCITY_HTTPS_PROXY_ENABLED=true environment variable to switch on alternative server-https-proxy.xml with enabled 'secure=true scheme=https' options
    So far, linux container.
    
    WIP: TW-68935 Insecure Tomcat connector attributes: missing secure attributes

--- context/run-server.sh
+++ context/run-server.sh
@@ -4,6 +4,7 @@
 : ${TEAMCITY_DIST:=/opt/teamcity}
 : ${TEAMCITY_LOGS:=${TEAMCITY_DIST}/logs}
 : ${TEAMCITY_CONTEXT:=ROOT}
+: ${TEAMCITY_SERVER_XML:="-config conf/server.xml"}
 : ${TEAMCITY_STOP_WAIT_TIME:=60}
 export TEAMCITY_LOGS
 
@@ -14,10 +15,16 @@ if [[ "$TEAMCITY_CONTEXT" != "ROOT" ]]; then
     [[ "$current" != "$TEAMCITY_CONTEXT" ]] && mv "${TEAMCITY_DIST}/webapps/$current" "${TEAMCITY_DIST}/webapps/$TEAMCITY_CONTEXT"
 fi
 
+if [[ "$TEAMCITY_HTTPS_PROXY_ENABLED" == "true" ]]; then
+  TEAMCITY_SERVER_XML="-config conf/server-https-proxy.xml"
+fi
+
 # Set traps to gently shutdown server on `docker stop`, `docker restart` or `docker kill -s 15`
 trap "'${TEAMCITY_DIST}/bin/teamcity-server.sh' stop ${TEAMCITY_STOP_WAIT_TIME} -force; exit \$?" TERM INT HUP
 
 # & and wait required for traps to work
-"${TEAMCITY_DIST}/bin/teamcity-server.sh" run &
+cd "${TEAMCITY_DIST}"
+echo "TeamCity server.xml parameter: $TEAMCITY_SERVER_XML"
+bin/teamcity-server.sh run $TEAMCITY_SERVER_XML &
 wait $!
 exit $?
