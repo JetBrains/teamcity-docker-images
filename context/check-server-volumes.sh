0bea2c35dfb897f3b4647e3ec2fe95caa336ed7c
Kirill (KIR) Maximov <kir@maxkir.com>
2020-12-15 13:13:13 +0100

    Fix grammar

--- context/check-server-volumes.sh
+++ context/check-server-volumes.sh
@@ -24,7 +24,7 @@ if [ "$PERMISSION_PROBLEM" = "1" ]; then
     cat <<END
 
     Looks like some mandatory directories are not writable (see above).
-    TeamCity container is run under '`whoami`' (`id -u`/`id -g`) user.
+    TeamCity container is running under '`whoami`' (`id -u`/`id -g`) user.
 
     A quick workaround: pass '-u 0' parameter to 'docker run' command to start it under 'root' user.
     The proper fix: run 'chown -R 1000:1000' on the corresponding volume(s), this can take noticeable time.
