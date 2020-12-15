5841fcca0053ea2006dffaf26be2bcee950c77cf
Kirill (KIR) Maximov <kir@maxkir.com>
2020-12-15 12:27:11 +0100

    Better warning message regarding non-writability for the teamcity-server image

--- context/check-server-volumes.sh
+++ context/check-server-volumes.sh
@@ -8,19 +8,27 @@ _check_dir() {
 
   touch "$DIR/.file.txt" 2>/dev/null
   if [ ! -f "$DIR/.file.txt" ]; then
-      echo ">>> $DIR_NAME '$DIR' is not a writeable directory"
+      echo ">>> Permission problem: $DIR_NAME '$DIR' is not a writeable directory"
       export PERMISSION_PROBLEM=1
   fi
   rm "$DIR/.file.txt" 2>/dev/null
 }
+
+echo ""
 
 _check_dir "$TEAMCITY_DATA_PATH" 'TEAMCITY_DATA_PATH'
 _check_dir "$TEAMCITY_LOGS" 'TEAMCITY_LOGS'
 _check_dir "$CATALINA_TMPDIR" 'CATALINA_TMPDIR'
 
 if [ "$PERMISSION_PROBLEM" = "1" ]; then
-    echo "    Current user is '`whoami`' (`id -u`/`id -g`)"
-    echo "    Check permissions and for the corresponding volume(s)."
-    echo "    If you're upgrading, chown -R 1000:1000 on the corresponding volume(s) directory may help."
+    cat <<END
+
+    Looks like some mandatory directories are not writable (see above).
+    TeamCity container is run under '`whoami`' (`id -u`/`id -g`) user.
+
+    A quick workaround: pass '-u 0' parameter to 'docker run' command to start it under 'root' user.
+    The proper fix: run 'chown -R 1000:1000' on the corresponding volume(s), this can take noticeable time.
+
+END
     exit 1
 fi
