c4f3397f7865c145971a6693066438ec60d99bbd
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2021-11-26 11:26:25 +0300

    Use .net 6 fot docker tool

--- tool/build-tool.cmd
+++ tool/build-tool.cmd
@@ -1,1 +1,1 @@
-if not exist "%~dp0..\bin\TeamCity.Docker.exe" %~dp0sdk dotnet publish "tool/TeamCity.Docker/TeamCity.Docker.csproj" -f netcoreapp3.1 -c release -r win-x64 --nologo -o bin /p:PublishSingleFile=true
+if not exist "%~dp0..\bin\TeamCity.Docker.exe" %~dp0sdk dotnet publish "tool/TeamCity.Docker/TeamCity.Docker.csproj" -f net6.0 -c release -r win-x64 --nologo --self-contained -o bin /p:PublishSingleFile=true
--- tool/sdk.sh
+++ tool/sdk.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #-H localhost:2375
-docker run -it --rm -w="/teamcity" -v "$(pwd):/teamcity" mcr.microsoft.com/dotnet/sdk:3.1 $@
+docker run -it --rm -w="/teamcity" -v "$(pwd):/teamcity" mcr.microsoft.com/dotnet/sdk:6.0 $@
