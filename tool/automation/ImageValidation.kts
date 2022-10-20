49db90c19f96fffead6085cdd437ada5debb33c9
AndreyKoltsov1997 <a99920641@gmail.com>
2022-10-20 12:32:41 +0200

    Add uncommited changes.

--- tool/automation/ImageValidation.kts
+++ tool/automation/ImageValidation.kts
@@ -1,9 +1,48 @@
+import java.util.concurrent.TimeUnit
+
 /**
- * You can edit, run, and share this code.
- * play.kotlinlang.org
+ * Executes command.
+ * @param command - command to be execution
+ * @param redirectStderr - indicates if error output must be captured along with ...
+ * ... stdout
+ * @return result of command's execution ; null in case of exception
  */
-fun main() {
+fun executeCommand(command: String, redirectStderr: Boolean, timeoutSec: Long = 60): String? {
+    return runCatching {
+        // -- converting command to list containing the arguments
+        val args = command.split(Regex("(?<!(\"|').{0,255}) | (?!.*\\1.*)"))
+        var builder = ProcessBuilder(args)
+            .redirectOutput(ProcessBuilder.Redirect.PIPE)
+            .apply {
+                // -- attach stderr-redirecting if required
+                if (redirectStderr) {
+                    this.redirectError(ProcessBuilder.Redirect.PIPE)
+                }
+            }
 
-    Runtime.getRuntime().exec("git status")
-
+        // -- execute command with timeout
+        builder.start().apply {waitFor(timeoutSec, TimeUnit.SECONDS)}
+            .inputStream.bufferedReader().readText()
+    }.onFailure {
+        it.printStackTrace()
+    }.getOrNull()
 }
+
+
+/**
+ * Retrieves docker image size.
+ * @param name image fully-qualified domain name
+ * @return image size in bytes, null in case image does not exist
+ */
+fun getDockerImageSize(name: String): String? {
+    var cmd = "docker inspect -f \"{{ .Size }}\" $name"
+    return this.executeCommand(cmd, true) ?: null
+}
+
+
+fun main() {
+    var res = this.getDockerImageSize("mcr.microsoft.com/dotnet/core/samples:dotnetapp-buster-slim")
+    print(res)
+}
+
+main()
