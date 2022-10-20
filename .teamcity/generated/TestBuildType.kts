3aa16663ee2302c4a2246a3c8638fa72ad176639
AndreyKoltsov1997 <a99920641@gmail.com>
2022-10-20 17:17:06 +0200

    Add missing import.

--- .teamcity/generated/TestBuildType.kts
+++ .teamcity/generated/TestBuildType.kts
@@ -1,14 +1,7 @@
 package generated
 
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.ui.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
-import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.dockerSupport
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.freeDiskSpace
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.swabra
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dockerCommand
 import common.TeamCityDockerImagesRepo.TeamCityDockerImagesRepo
+import jetbrains.buildServer.configs.kotlin.*
 
 object BuildAndPushHosted : BuildType({
     name = "Build and push for teamcity.jetbrains.com"
