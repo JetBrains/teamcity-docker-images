25b89ed25225310e0d9ffd4898125ebcdc739847
AndreyKoltsov1997 <a99920641@gmail.com>
2022-10-20 16:16:58 +0200

    Finish API for comparison between 2 images.

--- tool/automation/ImageValidation.kts
+++ tool/automation/ImageValidation.kts
@@ -1,4 +1,7 @@
+import java.lang.Exception
+import java.lang.NumberFormatException
 import java.lang.System
+import java.lang.Void
 import java.util.concurrent.TimeUnit
 
 /**
@@ -9,6 +12,7 @@ import java.util.concurrent.TimeUnit
  * @return result of command's execution ; null in case of exception
  */
 fun executeCommand(command: String, redirectStderr: Boolean, timeoutSec: Long = 60): String? {
+    println(command)
     return runCatching {
         // -- converting command to list containing the arguments
         val args = command.split(Regex("(?<!(\"|').{0,255}) | (?!.*\\1.*)"))
@@ -35,23 +39,65 @@ fun executeCommand(command: String, redirectStderr: Boolean, timeoutSec: Long = 
  * @param name image fully-qualified domain name
  * @return image size in bytes, null in case image does not exist
  */
-fun getDockerImageSize(name: String): String? {
-    val cmd = "docker inspect -f \"{{ .Size }}\" $name"
-    println(cmd)
-    return this.executeCommand(cmd, true) ?: null
+fun getDockerImageSize(name: String): Int? {
+    var cmdResult = this.executeCommand("docker inspect -f \"{{ .Size }}\" $name", true) ?: null
+    try {
+        // remove quotes from reult string
+        val imageSizeStr = cmdResult.toString().trim().replace("^\"|\"$".toRegex(), "")
+        return Integer.parseInt(imageSizeStr)
+
+    } catch (ex: Exception) {
+        System.err.println("Unable to convert size of image into an integer number: $cmdResult $ex")
+        return null
+    }
+}
+
+/**
+ * Tries to pull Docker image from registry.
+ * @param name - docker image fully-qualified domain name
+ * @return true if image had been successfully pulled, false otherwise
+ */
+fun pullDockerImage(name: String): Boolean {
+    val res = this.executeCommand("docker pull $name", true)
+    if (res == null) {
+        return false
+    }
+    return !(res.contains("Error response from daemon", ignoreCase = true))
+}
+
+
+fun verifyImageSizeRegression(currentName: String, previousName: String) {
+    // -- get size of current image
+    val curSize = this.getDockerImageSize(currentName)
+    if (curSize == null) {
+        println("Image does not exist on the agent: $currentName \n Perhaps image tag was not specified?")
+        return
+    }
+
+    // -- get size of previous image
+    val prevImagePullSucceeded = this.pullDockerImage(previousName)
+    val prevSize = this.getDockerImageSize(previousName)
+    if (!prevImagePullSucceeded || prevSize == null) {
+        println("Unable to get size of previous image: $previousName")
+        return
+    }
+
+    // TODO: change condition
+    if (curSize < prevSize) {
+        // TODO: change exception type
+        throw java.lang.Exception("image size is higher")
+    }
+    println("CurSize: $curSize, prevSize: $prevSize")
 }
 
 
 fun main(args: Array<String>) {
-    val imageName = args[0]
-    val res = this.getDockerImageSize(imageName)
-
-    if (res.isNullOrBlank()) {
-        println("Image does not exist on the agent: $imageName \n Perhaps image tag was not specified?")
-        return
+    if (args.size < 2) {
+        throw IllegalArgumentException("Not enough CLI arguments.")
     }
-
-    print(res)
+    val imageName = args[0]
+    val prevImageName = args[1]
+    verifyImageSizeRegression(imageName, prevImageName)
 }
 
 //  kotlinc -script tool/automation/ImageValidation.kts mcr.microsoft.com/dotnet/core/samples:dotnetapp-buster-slim
---
+++ tool/automation/README.md
@@ -0,0 +1,66 @@
+```
+
+kotlinScript {
+    content = """
+        import java.lang.System
+        import java.util.concurrent.TimeUnit
+        
+        /**
+         * Executes command.
+         * @param command - command to be execution
+         * @param redirectStderr - indicates if error output must be captured along with ...
+         * ... stdout
+         * @return result of command's execution ; null in case of exception
+         */
+        fun executeCommand(command: String, redirectStderr: Boolean, timeoutSec: Long = 60): String? {
+            return runCatching {
+                // -- converting command to list containing the arguments
+                val args = command.split(Regex("(?<!(\"|').{0,255}) | (?!.*\\1.*)"))
+                var builder = ProcessBuilder(args)
+                    .redirectOutput(ProcessBuilder.Redirect.PIPE)
+                    .apply {
+                        // -- attach stderr-redirecting if required
+                        if (redirectStderr) {
+                            this.redirectError(ProcessBuilder.Redirect.PIPE)
+                        }
+                    }
+        
+                // -- execute command with timeout
+                builder.start().apply {waitFor(timeoutSec, TimeUnit.SECONDS)}
+                    .inputStream.bufferedReader().readText()
+            }.onFailure {
+                it.printStackTrace()
+            }.getOrNull()
+        }
+        
+        
+        /**
+         * Retrieves docker image size.
+         * @param name image fully-qualified domain name
+         * @return image size in bytes, null in case image does not exist
+         */
+        fun getDockerImageSize(name: String): String? {
+            val cmd = "docker inspect -f \"{{ .Size }}\" ${'$'}name"
+            println(cmd)
+            return this.executeCommand(cmd, true) ?: null
+        }
+        
+        
+        fun main(args: Array<String>) {
+            val imageName = args[0]
+            val res = this.getDockerImageSize(imageName)
+        
+            if (res.isNullOrBlank()) {
+                println("Image does not exist on the agent: ${'$'}imageName \n Perhaps image tag was not specified?")
+                return
+            }
+        
+            print(res)
+        }
+        
+        //  kotlinc -script tool/automation/ImageValidation.kts mcr.microsoft.com/dotnet/core/samples:dotnetapp-buster-slim
+        main(args)
+    """.trimIndent()
+}
+
+```
