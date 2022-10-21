e1466eaeacc2dc68db80db811d5573e3a409d066
AndreyKoltsov1997 <a99920641@gmail.com>
2022-10-21 18:16:34 +0200

    Add stub for image prefix.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -16,6 +16,7 @@ object RootProject : Project({
         param("dockerImage.teamcity.buildNumber-1", "4")
         param("dep.TC_Trunk_BuildDistDocker.build.number", "")
         param("teamcity.ui.settings.readOnly", "false")
+        param("docker.buildImagePostfix", "")
 
         // Used by build for teamcity.jetbrains.com
         param("hostedLinuxVersion", "20.04")
