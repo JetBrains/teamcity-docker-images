0cbcb7bc90bf53b01aa7aaa8b73c9719af730a61
Andrey.Koltsov <andrey.koltsov@jetbrains.com>
2022-11-02 11:12:49 +0100

    Revert Kotlin settings.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import common.TeamCityDockerImagesRepo.TeamCityDockerImagesRepo
+import hosted.*
 import generated.*
 
 version = "2019.2"
@@ -8,14 +9,10 @@ object RootProject : Project({
     vcsRoot(TeamCityDockerImagesRepo)
     subProject(LocalProject.LocalProject)
     subProject(HubProject.HubProject)
-    buildType(TestBuildType.TestBuildType)
+    buildType(BuildAndPushHosted.BuildAndPushHosted)
     params {
-        param("dockerImage.teamcity.buildNumber", "4")
-        param("docker.buildRepository", "jetbrains/teamcity")
-
-        param("dep.TC_Trunk_BuildDistDocker.build.number", "%dep.TC2022_10_BuildDistDocker.build.number%")
+        param("dockerImage.teamcity.buildNumber", "%dep.TC_Trunk_BuildDistDocker.build.number%")
         param("teamcity.ui.settings.readOnly", "false")
-        param("docker.buildImagePostfix", "-staging")
 
         // Used by build for teamcity.jetbrains.com
         param("hostedLinuxVersion", "20.04")
