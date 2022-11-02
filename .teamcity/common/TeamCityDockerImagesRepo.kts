67f74a24f42ba3ded7c45a3d85637125164eae99
Andrey.Koltsov <andrey.koltsov@jetbrains.com>
2022-11-02 11:14:53 +0100

    Change image settings back to original.

--- .teamcity/common/TeamCityDockerImagesRepo.kts
+++ .teamcity/common/TeamCityDockerImagesRepo.kts
@@ -4,6 +4,6 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 object TeamCityDockerImagesRepo : GitVcsRoot({
     name = "TeamCity Docker Images"
-    url = "https://github.com/AndreyKoltsov1997/teamcity-docker-images.git"
-    branch = "refs/heads/TW-78090-add-docker-image-validation-test"
+    url = "https://github.com/JetBrains/teamcity-docker-images.git"
+    branch = "refs/heads/%teamcity.branch%"
 })
