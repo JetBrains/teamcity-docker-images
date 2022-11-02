c2279adb799e18cc663d12fc364d5d48bb3edd07
Andrey.Koltsov <andrey.koltsov@jetbrains.com>
2022-11-02 11:00:42 +0100

    Remove files not required for commit.

--- tool/TeamCity.Docker/generated/teamcity-agent-${versionTag}-nanoserver-1803.cmd
+++
@@ -1,9 +0,0 @@
-cd ..
-docker pull mcr.microsoft.com/powershell:nanoserver-1803
-docker pull mcr.microsoft.com/dotnet/framework/sdk:4.8-windowsservercore-1803
-echo TeamCity/webapps > context/.dockerignore
-echo TeamCity/devPackage >> context/.dockerignore
-echo TeamCity/lib >> context/.dockerignore
-docker build -f "generated/windows/MinimalAgent/nanoserver/1803/Dockerfile" -t teamcity-minimal-agent:${versionTag}-nanoserver-1803 "context"
-docker build -f "generated/windows/Agent/windowsservercore/1803/Dockerfile" -t teamcity-agent:${versionTag}-windowsservercore-1803 "context"
-docker build -f "generated/windows/Agent/nanoserver/1803/Dockerfile" -t teamcity-agent:${versionTag}-nanoserver-1803 "context"
--- tool/TeamCity.Docker/generated/teamcity-minimal-agent-${versionTag}-nanoserver-1809.cmd
+++
@@ -1,7 +0,0 @@
-cd ..
-docker pull mcr.microsoft.com/windows/nanoserver:1809
-docker pull mcr.microsoft.com/powershell:nanoserver-1809
-echo TeamCity/webapps > context/.dockerignore
-echo TeamCity/devPackage >> context/.dockerignore
-echo TeamCity/lib >> context/.dockerignore
-docker build -f "generated/windows/MinimalAgent/nanoserver/1809/Dockerfile" -t teamcity-minimal-agent:${versionTag}-nanoserver-1809 "context"
