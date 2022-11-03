227fe29f12c4746a48ae00f6e773380d3d2ed2c1
Andrey.Koltsov <andrey.koltsov@jetbrains.com>
2022-11-03 14:40:23 +0100

    Ensure boolean value is converted to lower string.

--- tool/TeamCity.Docker/TeamCityKotlinSettingsGenerator.cs
+++ tool/TeamCity.Docker/TeamCityKotlinSettingsGenerator.cs
@@ -789,8 +789,7 @@ namespace TeamCity.Docker
             yield return $"\t\t conditionType = {TeamCityConstants.Conditions.REGEXP}";
             yield return $"\t\t pattern = \"{pattern}\"";
             yield return "\t\t // allows the steps to continue running even in case of one problem";
-            // TODO: Ensure "false" is printed instead of "False"
-            yield return $"\t\t reportOnlyFirstMatch = {reportOnlyFirstMatch}";
+            yield return $"\t\t reportOnlyFirstMatch = {reportOnlyFirstMatch.ToString().ToLower()}";
             // end of "failOnText{...}
             yield return "\t }";
 
