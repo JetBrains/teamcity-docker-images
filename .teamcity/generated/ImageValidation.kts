a45e3da56bfdbd161d0e19a1bad1f43aa318f3c1
AndreyKoltsov1997 <a99920641@gmail.com>
2022-11-14 17:59:56 +0100

    Comment-out dependencies.

--- .teamcity/generated/ImageValidation.kts
+++ .teamcity/generated/ImageValidation.kts
@@ -132,20 +132,20 @@ steps {
 			 reportOnlyFirstMatch = false
 		 }
 	}
-	triggers {
-		 finishBuildTrigger {
-			 buildType = "${PublishHubVersion.publish_local.id}"
-			 branchFilter = "+:release/*|+:development/*"
-		 }
-	}
+//	triggers {
+//		 finishBuildTrigger {
+//			 buildType = "${PublishHubVersion.publish_local.id}"
+//			 branchFilter = "+:release/*|+:development/*"
+//		 }
+//	}
 
-	dependencies {
-		 dependency(AbsoluteId("TC_Trunk_DockerImages_push_hub_windows")) {
-			 snapshot { onDependencyFailure = FailureAction.ADD_PROBLEM }
-		 }
-		 dependency(AbsoluteId("TC_Trunk_DockerImages_push_hub_linux")) {
-			 snapshot { onDependencyFailure = FailureAction.ADD_PROBLEM }
-		 }
-	}
+//	dependencies {
+//		 dependency(AbsoluteId("TC_Trunk_DockerImages_push_hub_windows")) {
+//			 snapshot { onDependencyFailure = FailureAction.ADD_PROBLEM }
+//		 }
+//		 dependency(AbsoluteId("TC_Trunk_DockerImages_push_hub_linux")) {
+//			 snapshot { onDependencyFailure = FailureAction.ADD_PROBLEM }
+//		 }
+//	}
 })
 
