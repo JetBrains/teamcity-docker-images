5d43c39c65cf5d2cd402257597f31e3ce4647ebd
AndreyKoltsov1997 <a99920641@gmail.com>
2022-11-15 13:54:10 +0100

    Add more success messages @ Docker Image pull parsing.

--- tool/automation/ImageValidation.main.kts
+++ tool/automation/ImageValidation.main.kts
@@ -140,7 +140,7 @@ fun pullDockerImage(name: String): Boolean {
     println("Pulling $name ... \n $cmdResult")
     // using success messages since some errors from docker daemon (e.g. invalid platform type) are not ...
     // ... captured by Kotlin's ProcessBuilder.
-    val successMessages = arrayOf("Pull complete", "Image is up to date")
+    val successMessages = arrayOf("Pull complete", "Image is up to date", "Downloaded newer")
     return successMessages.any { cmdResult.contains(it, ignoreCase = true) }
 }
 
