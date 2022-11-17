02da18ca0dbea0cb04147684b1e5cbd1f60dcbd3
AndreyKoltsov1997 <a99920641@gmail.com>
2022-11-17 14:33:28 +0100

    Remove image not intended to verify.

--- .teamcity/generated/ImageValidationLinux.kts
+++ .teamcity/generated/ImageValidationLinux.kts
@@ -28,8 +28,7 @@ object image_validation_linux: BuildType(
 
         val images = listOf("%docker.deployRepository%teamcity-agent:2022.10-linux",
                             "%docker.deployRepository%teamcity-agent:2022.10-linux-sudo",
-                            "%docker.deployRepository%teamcity-minimal-agent:2022.10-linux",
-                            "%docker.deployRepository%teamcity-server:2022.10-nanoserver-1809")
+                            "%docker.deployRepository%teamcity-minimal-agent:2022.10-linux")
 
         steps {
             images.forEach {
