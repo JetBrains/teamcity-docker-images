543325b872392df9246d7102dfe6ad27edd74abd
AndreyKoltsov1997 <a99920641@gmail.com>
2023-01-31 13:18:41 +0100

    Update scheduled build of Windows-based images - add iteration over pre-defined list of images within the build.

--- .teamcity/hosted/scheduled/build/TeamCityScheduledImageBuildWindows.kts
+++ .teamcity/hosted/scheduled/build/TeamCityScheduledImageBuildWindows.kts
@@ -1,6 +1,7 @@
 package hosted.scheduled.build
 
 import common.TeamCityDockerImagesRepo.TeamCityDockerImagesRepo
+import hosted.scheduled.build.model.DockerImageInfo
 import jetbrains.buildServer.configs.kotlin.v2019_2.AbsoluteId
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2019_2.FailureAction
@@ -8,6 +9,8 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.ReuseBuilds
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.dockerSupport
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.DockerCommandStep
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dockerCommand
+import java.util.*
+import kotlin.collections.HashMap
 
 /**
  * Scheduled build of TeamCity Docker Images for Windows.
@@ -19,32 +22,68 @@ object TeamCityScheduledImageBuildWindows : BuildType({
         root(TeamCityDockerImagesRepo)
     }
 
-    // <image name> - <dockerfile path>
-    val targetImages: HashMap<String, String> = hashMapOf(
-        // Windows 1809
-        "teamcity-server-nanoserver-1809" to "context/generated/windows/Server/nanoserver/1809/Dockerfile",
-        "teamcity-minimal-agent-nanoserver-1809" to "context/generated/windows/MinimalAgent/nanoserver/1809/Dockerfile",
-        "teamcity-agent-windowsservercore-1809" to "context/generated/windows/Agent/windowsservercore/1809/Dockerfile",
-        "teamcity-agent-nanoserver-1809" to "context/generated/windows/Agent/nanoserver/1809/Dockerfile",
 
-        // Windows 20.04
-        "teamcity-server-nanoserver-2004" to "context/generated/windows/Server/nanoserver/2004/Dockerfile",
-        "teamcity-minimal-agent-nanoserver-2004" to "context/generated/windows/MinimalAgent/nanoserver/2004/Dockerfile",
-        "teamcity-agent-windowsservercore-2004" to "context/generated/windows/Agent/windowsservercore/2004/Dockerfile",
-        "teamcity-agent-nanoserver-2004" to "context/generated/windows/Agent/nanoserver/2004/Dockerfile",
+    val images = LinkedList(
+        listOf(
+            // Windows 18.09
+            DockerImageInfo(
+                "teamcity-server",
+                "EAP-nanoserver-1809",
+                "context/generated/windows/Server/nanoserver/1809/Dockerfile"
+            ),
+            DockerImageInfo(
+                "teamcity-minimal-agent",
+                "EAP-nanoserver-1809",
+                "context/generated/windows/MinimalAgent/nanoserver/1809/Dockerfile"
+            ),
+            DockerImageInfo(
+                "teamcity-agent",
+                "EAP-nanoserver-1809",
+                "context/generated/windows/Agent/nanoserver/1809/Dockerfile"
+            ),
+            // -- WindowsServerCore image is based on NanoServer image
+            DockerImageInfo(
+                "teamcity-agent",
+                "EAP-windowsservercore-1809",
+                "context/generated/windows/Agent/windowsservercore/1809/Dockerfile"
+            ),
+
+            // Windows 20.04
+            DockerImageInfo(
+                "teamcity-server",
+                "EAP-nanoserver-2004",
+                "context/generated/windows/Server/nanoserver/2004/Dockerfile"
+            ),
+            DockerImageInfo(
+                "teamcity-minimal-agent",
+                "EAP-nanoserver-2004",
+                "context/generated/windows/MinimalAgent/nanoserver/2004/Dockerfile"
+            ),
+            DockerImageInfo(
+                "teamcity-agent",
+                "EAP-nanoserver-2004",
+                "context/generated/windows/Agent/nanoserver/2004/Dockerfile"
+            ),
+            DockerImageInfo(
+                "teamcity-agent",
+                "EAP-windowsservercore-2004",
+                "context/generated/windows/Agent/windowsservercore/2004/Dockerfile"
+            )
+        )
     )
 
     steps {
-        targetImages.forEach { (imageName, dockerfilePath) ->
+
+        images.forEach { imageInfo ->
             dockerCommand {
-                name = "build $imageName"
+                name = "build ${imageInfo.repository}-${imageInfo.tag}"
                 commandType = build {
                     source = file {
-                        path = dockerfilePath
+                        path = imageInfo.dockerfilePath
                     }
-                    platform = DockerCommandStep.ImagePlatform.Windows
+                    platform = DockerCommandStep.ImagePlatform.Linux
                     contextDir = "context"
-                    namesAndTags = "${imageName}:%dockerImage.teamcity.buildNumber%"
+                    namesAndTags = "${imageInfo.repository}:${imageInfo.tag}"
                 }
             }
         }
