00976f5ac28abe64815d61b3f762357ba7037ac4
Kirill (KIR) Maximov <kir@maxkir.com>
2023-02-14 16:08:29 +0100

    TW-79512 Docker_in_docker does not restart reliable
    (cherry picked from commit 993cb5c2e60fb4734bc3b9bc7f12a4be60d8716c)

--- context/run-docker.sh
+++ context/run-docker.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 if [ "$DOCKER_IN_DOCKER" = "start" ] ; then
+
+ # Do cover the case when the container is restarted:
  sudo rm /var/run/docker.pid 2>/dev/null
+ sudo rm -rf /var/run/docker/containerd 2>/dev/null
+
  sudo service docker start
  echo "Docker daemon started"
  service docker status
