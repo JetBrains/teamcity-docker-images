dead0aa0c8f86e8ed044b33f6617a005d38065ea
AndreyKoltsov1997 <a99920641@gmail.com>
2023-05-22 17:52:59 +0200

    Update C# file for the download of JDK.

--- context/scripts/Program.cs
+++ context/scripts/Program.cs
@@ -5,8 +5,9 @@
         public static void Main()
         {
              Web.DownloadFiles(
-                 @"https://corretto.aws/downloads/resources/11.0.9.11.2/amazon-corretto-11.0.9.11.2-windows-x64-jdk.zip#MD5#9757ebcd8094b4d7040886b2c98bcf37",
+                 @"https://corretto.aws/downloads/resources/17.0.7.7.1/amazon-corretto-17.0.7.7.1-windows-x64-jdk.zip#MD5#feb7eab99c647a0b4347be9f0a3276de",
                  @"jdk.zip",
+                 // TODO: Add JRE 17
                  @"https://corretto.aws/downloads/resources/8.252.09.2/amazon-corretto-8.252.09.2-windows-x64-jre.zip",
                  @"jre.zip");
         }
