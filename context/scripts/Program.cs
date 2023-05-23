aa3e04398abb0b85dedde1553997fba48525cda4
AndreyKoltsov1997 <a99920641@gmail.com>
2023-05-23 19:18:14 +0200

    Replace JRE with JDK for Windows NanoServer images.

--- context/scripts/Program.cs
+++ context/scripts/Program.cs
@@ -7,8 +7,9 @@
              Web.DownloadFiles(
                  @"https://corretto.aws/downloads/resources/17.0.7.7.1/amazon-corretto-17.0.7.7.1-windows-x64-jdk.zip#MD5#feb7eab99c647a0b4347be9f0a3276de",
                  @"jdk.zip",
-                 // TODO: Add JRE 17
-                 @"https://corretto.aws/downloads/resources/8.252.09.2/amazon-corretto-8.252.09.2-windows-x64-jre.zip",
+                 // TODO: Replace mentions of JRE within NanoServer Docker manifests with JDK, which is actually used, given ...
+                 // ... missing Amazon Corretto's JRE 17 for Windows within official releases for Windows.
+                 @"https://corretto.aws/downloads/resources/17.0.7.7.1/amazon-corretto-17.0.7.7.1-windows-x64-jdk.zip#MD5#feb7eab99c647a0b4347be9f0a3276de",
                  @"jre.zip");
         }
     }
