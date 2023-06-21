4a1923e25081d71705b8bb43caaa4a1549dd51be
AndreyKoltsov1997 <a99920641@gmail.com>
2023-06-21 14:39:30 +0200

    Fix formatting & add EOL symbol.

--- .teamcity/hosted/scheduled/build/base/TeamCityScheduledImageBuildLinux_Base.kt
+++ .teamcity/hosted/scheduled/build/base/TeamCityScheduledImageBuildLinux_Base.kt
@@ -19,90 +19,89 @@ import java.lang.IllegalArgumentException
  * @param platform target platform, e.g. amd64, aarch64 (ARM)
  * @param agentReq a substring that agent name ('teamcity.agent.name') must contain
  */
-class TeamCityScheduledImageBuildLinux_Base(private val platform: String, private val agentReq: String) :
-    BuildType({
-        name = "[${platform}] TeamCity Docker Images - Automated Scheduled Build - Linux"
-        id("TeamCityScheduledImageBuildLinux_Base${platform}")
+class TeamCityScheduledImageBuildLinux_Base(private val platform: String, private val agentReq: String) : BuildType({
+    name = "[${platform}] TeamCity Docker Images - Automated Scheduled Build - Linux"
+    id("TeamCityScheduledImageBuildLinux_Base${platform}")
 
-        val images: Set<ImageInfo> = when {
-            platform.lowercase().contains("arm") || platform.lowercase()
-                .contains("aarch") -> ImageInfoRepository.getArmImages(
-                "%docker.nightlyRepository%",
-                "%dockerImage.teamcity.buildNumber%"
-            )
+    val images: Set<ImageInfo> = when {
+        platform.lowercase().contains("arm") || platform.lowercase()
+            .contains("aarch") -> ImageInfoRepository.getArmImages(
+            "%docker.nightlyRepository%",
+            "%dockerImage.teamcity.buildNumber%"
+        )
 
-            platform.lowercase().contains("amd") -> ImageInfoRepository.getAmdImages(
-                "%docker.nightlyRepository%",
-                "%dockerImage.teamcity.buildNumber%"
-            )
+        platform.lowercase().contains("amd") -> ImageInfoRepository.getAmdImages(
+            "%docker.nightlyRepository%",
+            "%dockerImage.teamcity.buildNumber%"
+        )
 
-            else -> throw IllegalArgumentException("Unable to find images for specified platform [${platform}]")
-        }
+        else -> throw IllegalArgumentException("Unable to find images for specified platform [${platform}]")
+    }
 
-        vcs {
-            root(TeamCityDockerImagesRepo.TeamCityDockerImagesRepo)
-        }
+    vcs {
+        root(TeamCityDockerImagesRepo.TeamCityDockerImagesRepo)
+    }
 
-        // all .yml files (e.g. compose samples)
-        artifactRules = "+:*.yml"
+    // all .yml files (e.g. compose samples)
+    artifactRules = "+:*.yml"
 
-        params {
-            // the images will be published into registry that holds nightly builds
-            param("docker.buildRepository", "%docker.nightlyRepository%")
-            // no postfix needed
-            param("docker.buildImagePostfix", "")
-            param("tc.image.version", "%dockerImage.teamcity.buildNumber%")
-        }
+    params {
+        // the images will be published into registry that holds nightly builds
+        param("docker.buildRepository", "%docker.nightlyRepository%")
+        // no postfix needed
+        param("docker.buildImagePostfix", "")
+        param("tc.image.version", "%dockerImage.teamcity.buildNumber%")
+    }
 
-        steps {
-            // build each image
-            images.forEach { imageInfo -> buildImage(imageInfo) }
+    steps {
+        // build each image
+        images.forEach { imageInfo -> buildImage(imageInfo) }
 
-            // publish images if build of each one of them succeeded
-            images.forEach { imageInfo -> publishImage(imageInfo) }
+        // publish images if build of each one of them succeeded
+        images.forEach { imageInfo -> publishImage(imageInfo) }
 
-            script {
-                name = "Generate Sample docker-compose manifest for the created images"
-                scriptContent = """
+        script {
+            name = "Generate Sample docker-compose manifest for the created images"
+            scriptContent = """
                 cat <<EOF > teamcity-linux-%tc.image.version%.docker-compose.${platform.trim()}.yml
                 ${Utils.getSampleComposeFile("%docker.nightlyRepository%", "%tc.image.version%", "", platform)}
                 EOF
                 """.trimIndent()
+        }
+    }
+
+    dependencies {
+        dependency(AbsoluteId("TC2023_05_BuildDistDocker")) {
+            artifacts {
+                artifactRules = "TeamCity.zip!/**=>context/TeamCity"
+                cleanDestination = true
+                lastSuccessful()
             }
         }
+    }
 
-        dependencies {
-            dependency(AbsoluteId("TC2023_05_BuildDistDocker")) {
-                artifacts {
-                    artifactRules = "TeamCity.zip!/**=>context/TeamCity"
-                    cleanDestination = true
-                    lastSuccessful()
-                }
+    features {
+        dockerSupport {
+            cleanupPushedImages = true
+            loginToRegistry = on {
+                dockerRegistryId = "PROJECT_EXT_315"
             }
         }
+    }
 
-        features {
-            dockerSupport {
-                cleanupPushedImages = true
-                loginToRegistry = on {
-                    dockerRegistryId = "PROJECT_EXT_315"
-                }
-            }
-        }
+    requirements {
+        // in case of aarch64, images must be built on aarch64-based agents
+        contains("teamcity.agent.name", agentReq)
+    }
 
-        requirements {
-            // in case of aarch64, images must be built on aarch64-based agents
-            contains("teamcity.agent.name", agentReq)
+    // We retain nightly builds for the previous 2 weeks
+    cleanup {
+        keepRule {
+            id = "TC_DOCKER_IMAGES_SCHEDULED_CLEANUP"
+            keepAtLeast = days(14)
+            dataToKeep = everything()
+            applyPerEachBranch = true
+            preserveArtifactsDependencies = true
         }
-
-        // We retain nightly builds for the previous 2 weeks
-        cleanup {
-            keepRule {
-                id = "TC_DOCKER_IMAGES_SCHEDULED_CLEANUP"
-                keepAtLeast = days(14)
-                dataToKeep = everything()
-                applyPerEachBranch = true
-                preserveArtifactsDependencies = true
-            }
-        }
-    })
+    }
+})
