cb8e22fc0f7e2f08415d3f829340ba4dc32a7254
AndreyKoltsov1997 <a99920641@gmail.com>
2023-07-11 09:48:38 +0200

    Update delivery config.

--- .teamcity/hosted/utils/config/DeliveryConfig.kt
+++ .teamcity/hosted/utils/config/DeliveryConfig.kt
@@ -6,7 +6,7 @@ package hosted.utils.config
 class DeliveryConfig {
     companion object {
         // Configuration within remote TeamCity Instance, thus the ID is stored as string
-        const val buildDistDockerDepId = "TC_2023_05_BuildDistDocker"
+        const val buildDistDockerDepId = "TC2023_05_BuildDistDocker"
 
         // Version must correspond to generated Dockerfiles, as minimal agent is a ...
         // ... base image for regular agent.
