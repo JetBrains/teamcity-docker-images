0e60b04e52ddbee8d3ec12756ac6ecf7953220d9
AndreyKoltsov1997 <a99920641@gmail.com>
2023-07-21 12:13:15 +0200

    Update generated context: scripts, documentation.

---
+++ context/generated/teamcity-server-2023.05.2-nanoserver-1803.cmd
@@ -0,0 +1,5 @@
+cd ../..
+docker pull mcr.microsoft.com/powershell:nanoserver-1803
+echo TeamCity/buildAgent > context/.dockerignore
+echo TeamCity/temp >> context/.dockerignore
+docker build -f "context/generated/windows/Server/nanoserver/1803/Dockerfile" -t teamcity-server:2023.05.2-nanoserver-1803 "context"
