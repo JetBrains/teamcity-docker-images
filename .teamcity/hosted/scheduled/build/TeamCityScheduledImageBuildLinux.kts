96d70fdc621d32cc946d88b8e353bbc0289db5b8
AndreyKoltsov1997 <a99920641@gmail.com>
2023-07-21 12:22:56 +0200

    Update delivery configurations.

--- .teamcity/hosted/scheduled/build/TeamCityScheduledImageBuildLinux.kts
+++ .teamcity/hosted/scheduled/build/TeamCityScheduledImageBuildLinux.kts
@@ -31,7 +31,7 @@ object TeamCityScheduledImageBuildLinux : BuildType({
         param("docker.buildImagePostfix", "")
 
         // 'trunk' could be replaced with a particular release number, such as 2023.05.
-        param("tc.image.version", "2023.05.1-%dockerImage.teamcity.buildNumber%")
+        param("tc.image.version", "2023.05.2-%dockerImage.teamcity.buildNumber%")
     }
 
     steps {
