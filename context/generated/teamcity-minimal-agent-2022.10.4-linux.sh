4e2a48b824d52eecef78adc5ab4778b4b791a7e7
AndreyKoltsov1997 <a99920641@gmail.com>
2023-09-11 16:38:22 +0200

    Update scripts for the generation of Docker images.

--- context/generated/teamcity-minimal-agent-2022.10.4-linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ../..
-docker pull ubuntu:20.04
-echo TeamCity/webapps > context/.dockerignore
-echo TeamCity/devPackage >> context/.dockerignore
-echo TeamCity/lib >> context/.dockerignore
-docker build -f "context/generated/linux/MinimalAgent/Ubuntu/20.04/Dockerfile" -t teamcity-minimal-agent:2022.10.4-linux "context"
