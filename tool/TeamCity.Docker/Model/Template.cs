1cdac696f3f5a8a15a492b27ee8ca858eeda4a8e
AndreyKoltsov1997 <a99920641@gmail.com>
2023-11-09 13:29:07 +0100

    Add docstrings.

--- tool/TeamCity.Docker/Model/Template.cs
+++ tool/TeamCity.Docker/Model/Template.cs
@@ -10,6 +10,12 @@
         [NotNull] public readonly IEnumerable<Variant> Variants;
         [NotNull] public readonly IReadOnlyCollection<string> Ignore;
 
+        /// <summary>
+        /// Creates object describing configuration for Dockerfile.
+        /// </summary>
+        /// <param name="lines">Content of the template Dockerfile.</param>
+        /// <param name="variants">Different configuration options (.config files)</param>
+        /// <param name="ignore">Content of .Dockerignore</param>
         public Template([NotNull] IReadOnlyCollection<string> lines, [NotNull] IReadOnlyCollection<Variant> variants, [NotNull] IReadOnlyCollection<string> ignore)
         {
             Lines = lines ?? throw new ArgumentNullException(nameof(lines));
