647ff20ee637847dcea5cd991c0b9316a7ce6db0
AndreyKoltsov1997 <a99920641@gmail.com>
2023-11-20 21:17:30 +0100

    [TCQA] Remove debug logic added due to merge conflict.

---
+++ context/generated/teamcity-agent-2023.11-nanoserver-2022.cmd
@@ -0,0 +1,10 @@
+cd ../..
+docker pull mcr.microsoft.com/windows/nanoserver:ltsc2022
+docker pull mcr.microsoft.com/powershell:nanoserver-ltsc2022
+docker pull mcr.microsoft.com/dotnet/framework/sdk:4.8-windowsservercore-ltsc2022
+echo TeamCity/webapps > context/.dockerignore
+echo TeamCity/devPackage >> context/.dockerignore
+echo TeamCity/lib >> context/.dockerignore
+docker build -f "context/generated/windows/MinimalAgent/nanoserver/2022/Dockerfile" -t teamcity-minimal-agent:2023.11-nanoserver-2022 "context"
+docker build -f "context/generated/windows/Agent/windowsservercore/2022/Dockerfile" -t teamcity-agent:2023.11-windowsservercore-2022 "context"
+docker build -f "context/generated/windows/Agent/nanoserver/2022/Dockerfile" -t teamcity-agent:2023.11-nanoserver-2022 "context"
