37de26e02db24c2b6527d2889d47ecc268479a9f
AndreyKoltsov1997 <a99920641@gmail.com>
2024-01-12 12:15:22 +0100

    [TCQA] Update versioning within 2023.11.x branch to reflect the upcoming release.

---
+++ context/generated/teamcity-server-2023.11.2-nanoserver-1809.cmd
@@ -0,0 +1,5 @@
+cd ../..
+docker pull mcr.microsoft.com/powershell:nanoserver-1809
+echo TeamCity/buildAgent > context/.dockerignore
+echo TeamCity/temp >> context/.dockerignore
+docker build -f "context/generated/windows/Server/nanoserver/1809/Dockerfile" -t teamcity-server:2023.11.2-nanoserver-1809 "context"
