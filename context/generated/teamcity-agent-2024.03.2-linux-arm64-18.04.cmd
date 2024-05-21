b1ef71a5c9ea5fdbc8fde058dfe181cb374edf6d
AndreyKoltsov1997 <a99920641@gmail.com>
2024-05-21 15:36:43 +0200

    Update version: 2024.03.1 -> 2024.03.2

---
+++ context/generated/teamcity-agent-2024.03.2-linux-arm64-18.04.cmd
@@ -0,0 +1,9 @@
+cd ../..
+docker pull ubuntu:18.04
+echo TeamCity/webapps > context/.dockerignore
+echo TeamCity/devPackage >> context/.dockerignore
+echo TeamCity/lib >> context/.dockerignore
+docker build -f "context/generated/linux/MinimalAgent/UbuntuARM/18.04/Dockerfile" -t teamcity-minimal-agent:2024.03.2-linux-arm64-18.04 "context"
+echo 2> context/.dockerignore
+echo TeamCity >> context/.dockerignore
+docker build -f "context/generated/linux/Agent/UbuntuARM/18.04/Dockerfile" -t teamcity-agent:2024.03.2-linux-arm64-18.04 "context"
---
+++ context/generated/teamcity-minimal-agent-2024.03.2-nanoserver-1803.cmd
@@ -0,0 +1,6 @@
+cd ../..
+docker pull mcr.microsoft.com/powershell:nanoserver-1803
+echo TeamCity/webapps > context/.dockerignore
+echo TeamCity/devPackage >> context/.dockerignore
+echo TeamCity/lib >> context/.dockerignore
+docker build -f "context/generated/windows/MinimalAgent/nanoserver/1803/Dockerfile" -t teamcity-minimal-agent:2024.03.2-nanoserver-1803 "context"
