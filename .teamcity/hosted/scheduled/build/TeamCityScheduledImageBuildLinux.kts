a44dc5fa05107d0ada4c6bc945ee2ea1b5edd8a8
AndreyKoltsov1997 <a99920641@gmail.com>
2024-05-21 13:55:32 +0200

    Update versioning within DSL constants.

--- .teamcity/hosted/scheduled/build/TeamCityScheduledImageBuildLinux.kts
+++ .teamcity/hosted/scheduled/build/TeamCityScheduledImageBuildLinux.kts
@@ -31,7 +31,7 @@ object TeamCityScheduledImageBuildLinux : BuildType({
         param("docker.buildImagePostfix", "")
 
         // 'trunk' could be replaced with a particular release number, such as 2023.05.
-        param("tc.image.version", "2023.05.4-%dockerImage.teamcity.buildNumber%")
+        param("tc.image.version", "2023.05.5-%dockerImage.teamcity.buildNumber%")
     }
 
     steps {
