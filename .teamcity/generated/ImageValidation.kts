71e026313b0ab5dc87019febbf4d7c4d43a00091
AndreyKoltsov1997 <a99920641@gmail.com>
2024-05-21 14:52:19 +0200

    Re-generate DSL.

--- .teamcity/generated/ImageValidation.kts
+++ .teamcity/generated/ImageValidation.kts
@@ -24,87 +24,86 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.finishBuildTrigger
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 object image_validation: BuildType({
-	 name = "Validation of Size Regression - Staging Docker Images (Windows / Linux)"
-	 buildNumberPattern="%dockerImage.teamcity.buildNumber%-%build.counter%"
-	 vcs {
-		 root(TeamCityDockerImagesRepo)
-	 }
+	name = "Validation of Size Regression - Staging Docker Images (Windows / Linux)"
+	buildNumberPattern="%dockerImage.teamcity.buildNumber%-%build.counter%"
+	vcs {
+		root(TeamCityDockerImagesRepo)
+	}
 
-	 triggers {
-		 // Execute the build once the images are available within %deployRepository%
-		 finishBuildTrigger {
-			 buildType = "${PublishHubVersion.publish_hub_version.id}"
-			 // if filter won't be specified, only <default> branch would be included
-			  branchFilter = """
+	triggers {
+		// Execute the build once the images are available within %deployRepository%
+		finishBuildTrigger {
+			buildType = "${PublishHubVersion.publish_hub_version.id}"
+			// if filter won't be specified, only <default> branch would be included
+			branchFilter = """
 				 +:development/*
 				 +:release/*
 			 """.trimIndent()
-		 }
-	 }
+		}
+	}
 
-	 params {
-		 // -- inherited parameter, removed in debug purposes
-		 param("dockerImage.teamcity.buildNumber", "-")
-	 }
+	params {
+		// -- inherited parameter, removed in debug purposes
+		param("dockerImage.teamcity.buildNumber", "-")
+	}
 
-	 val targetImages: HashMap<String, String> = hashMapOf(
-"teamcity-server-2022.04.5-linux" to "%docker.deployRepository%teamcity-server%docker.buildImagePostfix%:2022.04.5-linux", 
-		"teamcity-agent-2022.04.5-linux" to "%docker.deployRepository%teamcity-agent%docker.buildImagePostfix%:2022.04.5-linux", 
-		"teamcity-agent-2022.04.5-linux-sudo" to "%docker.deployRepository%teamcity-agent%docker.buildImagePostfix%:2022.04.5-linux-sudo", 
-		"teamcity-minimal-agent-2022.04.5-linux" to "%docker.deployRepository%teamcity-minimal-agent%docker.buildImagePostfix%:2022.04.5-linux", 
-		"teamcity-server-2022.04.5-nanoserver-1809" to "%docker.deployRepository%teamcity-server%docker.buildImagePostfix%:2022.04.5-nanoserver-1809", 
-		"teamcity-agent-2022.04.5-windowsservercore-1809" to "%docker.deployRepository%teamcity-agent%docker.buildImagePostfix%:2022.04.5-windowsservercore-1809", 
-		"teamcity-agent-2022.04.5-nanoserver-1809" to "%docker.deployRepository%teamcity-agent%docker.buildImagePostfix%:2022.04.5-nanoserver-1809", 
-		"teamcity-minimal-agent-2022.04.5-nanoserver-1809" to "%docker.deployRepository%teamcity-minimal-agent%docker.buildImagePostfix%:2022.04.5-nanoserver-1809", 
-		"teamcity-server-2022.04.5-nanoserver-2004" to "%docker.deployRepository%teamcity-server%docker.buildImagePostfix%:2022.04.5-nanoserver-2004", 
-		"teamcity-agent-2022.04.5-windowsservercore-2004" to "%docker.deployRepository%teamcity-agent%docker.buildImagePostfix%:2022.04.5-windowsservercore-2004", 
-		"teamcity-agent-2022.04.5-nanoserver-2004" to "%docker.deployRepository%teamcity-agent%docker.buildImagePostfix%:2022.04.5-nanoserver-2004", 
-		"teamcity-minimal-agent-2022.04.5-nanoserver-2004" to "%docker.deployRepository%teamcity-minimal-agent%docker.buildImagePostfix%:2022.04.5-nanoserver-2004"
-	  )
+	val targetImages: HashMap<String, String> = hashMapOf(
+		"teamcity-server-2022.04.6-linux" to "%docker.deployRepository%teamcity-server%docker.buildImagePostfix%:2022.04.6-linux",
+		"teamcity-agent-2022.04.6-linux" to "%docker.deployRepository%teamcity-agent%docker.buildImagePostfix%:2022.04.6-linux",
+		"teamcity-agent-2022.04.6-linux-sudo" to "%docker.deployRepository%teamcity-agent%docker.buildImagePostfix%:2022.04.6-linux-sudo",
+		"teamcity-minimal-agent-2022.04.6-linux" to "%docker.deployRepository%teamcity-minimal-agent%docker.buildImagePostfix%:2022.04.6-linux",
+		"teamcity-server-2022.04.6-nanoserver-1809" to "%docker.deployRepository%teamcity-server%docker.buildImagePostfix%:2022.04.6-nanoserver-1809",
+		"teamcity-agent-2022.04.6-windowsservercore-1809" to "%docker.deployRepository%teamcity-agent%docker.buildImagePostfix%:2022.04.6-windowsservercore-1809",
+		"teamcity-agent-2022.04.6-nanoserver-1809" to "%docker.deployRepository%teamcity-agent%docker.buildImagePostfix%:2022.04.6-nanoserver-1809",
+		"teamcity-minimal-agent-2022.04.6-nanoserver-1809" to "%docker.deployRepository%teamcity-minimal-agent%docker.buildImagePostfix%:2022.04.6-nanoserver-1809",
+		"teamcity-server-2022.04.6-nanoserver-2004" to "%docker.deployRepository%teamcity-server%docker.buildImagePostfix%:2022.04.6-nanoserver-2004",
+		"teamcity-agent-2022.04.6-windowsservercore-2004" to "%docker.deployRepository%teamcity-agent%docker.buildImagePostfix%:2022.04.6-windowsservercore-2004",
+		"teamcity-agent-2022.04.6-nanoserver-2004" to "%docker.deployRepository%teamcity-agent%docker.buildImagePostfix%:2022.04.6-nanoserver-2004",
+		"teamcity-minimal-agent-2022.04.6-nanoserver-2004" to "%docker.deployRepository%teamcity-minimal-agent%docker.buildImagePostfix%:2022.04.6-nanoserver-2004"
+	)
 
-	 steps {
-		   targetImages.forEach { (imageVerificationStepId, imageDomainName) ->
-		     // Generate validation for each image fully-qualified domain name (FQDN)
-		     gradle {
-			       name = "Image Verification - $imageVerificationStepId"
-			       tasks = "clean build run --args=\"validate  $imageDomainName %docker.stagingRepository.login% %docker.stagingRepository.token%\""
-			       workingDir = "tool/automation/framework"
-			       buildFile = "build.gradle"
-			       jdkHome = "%env.JDK_11_x64%"
-			       executionMode = BuildStep.ExecutionMode.ALWAYS
-			     }
-		   }
-	 }
+	steps {
+		targetImages.forEach { (imageVerificationStepId, imageDomainName) ->
+			// Generate validation for each image fully-qualified domain name (FQDN)
+			gradle {
+				name = "Image Verification - $imageVerificationStepId"
+				tasks = "clean build run --args=\"validate  $imageDomainName %docker.stagingRepository.login% %docker.stagingRepository.token%\""
+				workingDir = "tool/automation/framework"
+				buildFile = "build.gradle"
+				jdkHome = "%env.JDK_11_x64%"
+				executionMode = BuildStep.ExecutionMode.ALWAYS
+			}
+		}
+	}
 
-	 failureConditions {
-		   // Failed in case the validation via framework didn't succeed
-		   failOnText {
-			     conditionType = BuildFailureOnText.ConditionType.CONTAINS
-			     pattern = "DockerImageValidationException"
-			     failureMessage = "Docker Image validation have failed"
-			     // allows the steps to continue running even in case of one problem
-			     reportOnlyFirstMatch = false
-		   }
-	 }
+	failureConditions {
+		// Failed in case the validation via framework didn't succeed
+		failOnText {
+			conditionType = BuildFailureOnText.ConditionType.CONTAINS
+			pattern = "DockerImageValidationException"
+			failureMessage = "Docker Image validation have failed"
+			// allows the steps to continue running even in case of one problem
+			reportOnlyFirstMatch = false
+		}
+	}
 
-	 requirements {
-		  exists("env.JDK_11")
-		  // Images are validated mostly via DockerHub REST API. In case ...
-		  // ... Docker agent will be used, platform-compatibility must be addressed, ...
-		  // ... especially in case of Windows images.
-		  contains("teamcity.agent.jvm.os.name", "Linux")
+	requirements {
+		exists("env.JDK_11")
+		// Images are validated mostly via DockerHub REST API. In case ...
+		// ... Docker agent will be used, platform-compatibility must be addressed, ...
+		// ... especially in case of Windows images.
+		contains("teamcity.agent.jvm.os.name", "Linux")
 
-		  // Currently, only AMD-based images are published
-		  doesNotContain("teamcity.agent.name", "arm")
-	 }
+		// Currently, only AMD-based images are published
+		doesNotContain("teamcity.agent.name", "arm")
+	}
 
-	 features {
-		   dockerSupport {
-			     cleanupPushedImages = true
-			     loginToRegistry = on {
-			       dockerRegistryId = "PROJECT_EXT_774"
-			     }
-		   }
-	 }
+	features {
+		dockerSupport {
+			cleanupPushedImages = true
+			loginToRegistry = on {
+				dockerRegistryId = "PROJECT_EXT_774"
+			}
+		}
+	}
 })
-
