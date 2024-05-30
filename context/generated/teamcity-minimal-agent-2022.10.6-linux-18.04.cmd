4e14d245540773093430449aa99f165b51cd843e
AndreyKoltsov1997 <a99920641@gmail.com>
2024-05-30 11:06:27 +0200

    Update version to 2022.10.6 & re-generate dockerfiles.

---
+++ context/generated/teamcity-minimal-agent-2022.10.6-linux-18.04.cmd
@@ -0,0 +1,6 @@
+cd ../..
+docker pull ubuntu:18.04
+echo TeamCity/webapps > context/.dockerignore
+echo TeamCity/devPackage >> context/.dockerignore
+echo TeamCity/lib >> context/.dockerignore
+docker build -f "context/generated/linux/MinimalAgent/Ubuntu/18.04/Dockerfile" -t teamcity-minimal-agent:2022.10.6-linux-18.04 "context"
