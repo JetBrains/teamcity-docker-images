260faad95bb891a8af4a60615da0aa5cefc60ea7
AndreyKoltsov1997 <a99920641@gmail.com>
2024-05-30 11:14:18 +0200

    Update version & re-generate dockerfiles.

--- context/generated/teamcity-server-2023.05.5-linux-arm64.cmd
+++
@@ -1,5 +0,0 @@
-cd ../..
-docker pull ubuntu:20.04
-echo TeamCity/buildAgent > context/.dockerignore
-echo TeamCity/temp >> context/.dockerignore
-docker build -f "context/generated/linux/Server/UbuntuARM/20.04/Dockerfile" -t teamcity-server:2023.05.5-linux-arm64 "context"
