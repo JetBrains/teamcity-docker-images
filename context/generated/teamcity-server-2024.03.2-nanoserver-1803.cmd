88956d894d8373e18c500ae01a5ba6ce5a6f733b
AndreyKoltsov1997 <a99920641@gmail.com>
2024-06-26 18:03:20 +0200

    Update versioning to 2023.03.3

--- context/generated/teamcity-server-2024.03.2-nanoserver-1803.cmd
+++
@@ -1,5 +0,0 @@
-cd ../..
-docker pull mcr.microsoft.com/powershell:nanoserver-1803
-echo TeamCity/buildAgent > context/.dockerignore
-echo TeamCity/temp >> context/.dockerignore
-docker build -f "context/generated/windows/Server/nanoserver/1803/Dockerfile" -t teamcity-server:2024.03.2-nanoserver-1803 "context"
