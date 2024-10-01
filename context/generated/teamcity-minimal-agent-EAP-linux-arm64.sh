0ec8634822b3963d09de7ffb2c3bae4edb95a431
AndreyKoltsov1997 <a99920641@gmail.com>
2024-10-01 15:16:27 +0200

    Re-generate scripts.

--- context/generated/teamcity-minimal-agent-EAP-linux-arm64.sh
+++ context/generated/teamcity-minimal-agent-EAP-linux-arm64.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ../..
-docker pull ubuntu:20.04
+docker pull ubuntu:22.04
 echo TeamCity/webapps > context/.dockerignore
 echo TeamCity/devPackage >> context/.dockerignore
 echo TeamCity/lib >> context/.dockerignore
-docker build -f "context/generated/linux/MinimalAgent/UbuntuARM/20.04/Dockerfile" -t teamcity-minimal-agent:EAP-linux-arm64 "context"
+docker build -f "context/generated/linux/MinimalAgent/UbuntuARM/22.04/Dockerfile" -t teamcity-minimal-agent:EAP-linux-arm64 "context"
