6574910dc0d247b14fa3683a29d07de05e692e99
AndreyKoltsov1997 <a99920641@gmail.com>
2024-10-09 13:30:54 +0200

    * Add configuration & generate dockerfiles for regular agents, sudo agents & servers.

--- context/generated/teamcity-agent-EAP-linux-sudo.sh
+++ context/generated/teamcity-agent-EAP-linux-sudo.sh
@@ -7,5 +7,5 @@ echo TeamCity/lib >> context/.dockerignore
 docker build -f "context/generated/linux/MinimalAgent/Ubuntu/22.04/Dockerfile" -t teamcity-minimal-agent:EAP-linux "context"
 echo 2> context/.dockerignore
 echo TeamCity >> context/.dockerignore
-docker build -f "context/generated/linux/Agent/Ubuntu/20.04/Dockerfile" -t teamcity-agent:EAP-linux "context"
+docker build -f "context/generated/linux/Agent/Ubuntu/22.04/Dockerfile" -t teamcity-agent:EAP-linux "context"
 docker build -f "context/generated/linux/Agent/Ubuntu/20.04-sudo/Dockerfile" -t teamcity-agent:EAP-linux-sudo "context"
