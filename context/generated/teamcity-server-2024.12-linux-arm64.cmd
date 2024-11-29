82e976d1bebff5905e4844b4ed983d07c55c9420
AndreyKoltsov1997 <a99920641@gmail.com>
2024-11-29 10:20:01 +0100

    Re-generate .sh and .cmd scripts

---
+++ context/generated/teamcity-server-2024.12-linux-arm64.cmd
@@ -0,0 +1,5 @@
+cd ../..
+docker pull ubuntu:22.04
+echo TeamCity/buildAgent > context/.dockerignore
+echo TeamCity/temp >> context/.dockerignore
+docker build -f "context/generated/linux/Server/UbuntuARM/22.04/Dockerfile" -t teamcity-server:2024.12-linux-arm64 "context"
---
+++ context/generated/teamcity-server-2024.12-nanoserver-1903.cmd
@@ -0,0 +1,5 @@
+cd ../..
+docker pull mcr.microsoft.com/powershell:nanoserver-1903
+echo TeamCity/buildAgent > context/.dockerignore
+echo TeamCity/temp >> context/.dockerignore
+docker build -f "context/generated/windows/Server/nanoserver/1903/Dockerfile" -t teamcity-server:2024.12-nanoserver-1903 "context"
