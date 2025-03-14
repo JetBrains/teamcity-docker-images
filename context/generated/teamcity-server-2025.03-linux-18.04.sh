462f004e6e60a7732bcb5c8fbcb48f5a02266bc0
Andrey Koltsov <a99920641@gmail.com>
2025-03-14 20:16:04 +0100

    Update versioning (#195)
    [TW] 2025.03: update versioning for the upcoming release #195

---
+++ context/generated/teamcity-server-2025.03-linux-18.04.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ../..
+docker pull ubuntu:18.04
+echo TeamCity/buildAgent > context/.dockerignore
+echo TeamCity/temp >> context/.dockerignore
+docker build -f "context/generated/linux/Server/Ubuntu/18.04/Dockerfile" -t teamcity-server:2025.03-linux-18.04 "context"
