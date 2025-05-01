f814a4ac4c1198820ffbd4e7cc2584ff1cd7d060
nastasia.chubatova <nastasia.chubatova@jetbrains.com>
2025-05-01 21:39:33 +0200

    Docker images v. 2025.03.2

---
+++ context/generated/teamcity-server-2025.03.2-nanoserver-1803.cmd
@@ -0,0 +1,5 @@
+cd ../..
+docker pull mcr.microsoft.com/powershell:nanoserver-1803
+echo TeamCity/buildAgent > context/.dockerignore
+echo TeamCity/temp >> context/.dockerignore
+docker build -f "context/generated/windows/Server/nanoserver/1803/Dockerfile" -t teamcity-server:2025.03.2-nanoserver-1803 "context"
