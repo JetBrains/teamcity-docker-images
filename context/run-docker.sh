1aac0a1c98daa8e32021508f1527d015b3aee00c
AndreyKoltsov1997 <a99920641@gmail.com>
2025-09-05 11:40:03 +0200

    Add an ability to optionally run DinD with legacy IP Tables.

--- context/run-docker.sh
+++ context/run-docker.sh
@@ -2,6 +2,23 @@
 
 if [ "$DOCKER_IN_DOCKER" = "start" ] ; then
 
+# By default, Docker in containers uses nftables. However, the host may be using legacy iptables.
+# This section optionally switches the container to use legacy iptables for Docker-in-Docker compatibility. See: TW-94273
+  if [ "$DOCKER_IPTABLES_LEGACY" == "1" ]; then
+       # Switch IPTables-related tools to legacy tables
+       sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+       sudo update-alternatives --install /usr/bin/iptables iptables /usr/sbin/iptables-nft 30
+       sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+
+       # Disable conflicting services
+       sudo systemctl disable --now ufw
+       sudo systemctl disable --now nftables
+       sudo apt update && apt install -t iptables-persistent
+
+       # Enable legacy tables by defaults
+       sudo systemctl enable --now iptables
+  fi
+
  # Do cover the case when the container is restarted:
  sudo rm /var/run/docker.pid 2>/dev/null
  sudo rm -rf /var/run/docker/containerd 2>/dev/null
