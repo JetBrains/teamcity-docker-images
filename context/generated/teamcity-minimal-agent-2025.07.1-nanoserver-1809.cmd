3e824b41dbd379e09f9c356f7ca0dd3313ba1e79
Olga Sventukh <olga.sventukh@jetbrains.com>
2025-09-08 15:19:25 +0200

    Update-the-version-to-2025.07.2 (#212)
    * Update Version 2025.07.1 -> 2025.07.2

--- context/generated/teamcity-minimal-agent-2025.07.1-nanoserver-1809.cmd
+++
@@ -1,7 +0,0 @@
-cd ../..
-docker pull mcr.microsoft.com/windows/nanoserver:1809
-docker pull mcr.microsoft.com/powershell:nanoserver-1809
-echo TeamCity/webapps > context/.dockerignore
-echo TeamCity/devPackage >> context/.dockerignore
-echo TeamCity/lib >> context/.dockerignore
-docker build -f "context/generated/windows/MinimalAgent/nanoserver/1809/Dockerfile" -t teamcity-minimal-agent:2025.07.1-nanoserver-1809 "context"
---
+++ context/generated/teamcity-server-2025.07.2-linux-arm64-18.04.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ../..
+docker pull ubuntu:18.04
+echo TeamCity/buildAgent > context/.dockerignore
+echo TeamCity/temp >> context/.dockerignore
+docker build -f "context/generated/linux/Server/UbuntuARM/18.04/Dockerfile" -t teamcity-server:2025.07.2-linux-arm64-18.04 "context"
