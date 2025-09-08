5f1f9e081826e24f5dd60fdd5b4caf1238605c73
ollven <ollven@yandex.ru>
2025-09-08 13:24:22 +0200

    Update Version 2025.07.1 -> 2025.07.2

--- context/generated/teamcity-minimal-agent-2025.07.1-linux-arm64-18.04.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ../..
-docker pull ubuntu:18.04
-echo TeamCity/webapps > context/.dockerignore
-echo TeamCity/devPackage >> context/.dockerignore
-echo TeamCity/lib >> context/.dockerignore
-docker build -f "context/generated/linux/MinimalAgent/UbuntuARM/18.04/Dockerfile" -t teamcity-minimal-agent:2025.07.1-linux-arm64-18.04 "context"
