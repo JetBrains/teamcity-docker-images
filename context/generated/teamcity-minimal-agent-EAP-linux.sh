c814b7e771a8dec34f9cc4a5d7f1111a15245707
AndreyKoltsov1997 <a99920641@gmail.com>
2025-11-03 13:37:54 +0100

    Update scripts to reflect the new version

--- context/generated/teamcity-minimal-agent-EAP-linux.sh
+++ context/generated/teamcity-minimal-agent-EAP-linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ../..
-docker pull ubuntu:22.04
+docker pull ubuntu:24.04
 echo TeamCity/webapps > context/.dockerignore
 echo TeamCity/devPackage >> context/.dockerignore
 echo TeamCity/lib >> context/.dockerignore
-docker build -f "context/generated/linux/MinimalAgent/Ubuntu/22.04/Dockerfile" -t teamcity-minimal-agent:EAP-linux "context"
+docker build -f "context/generated/linux/MinimalAgent/Ubuntu/24.04/Dockerfile" -t teamcity-minimal-agent:EAP-linux "context"
