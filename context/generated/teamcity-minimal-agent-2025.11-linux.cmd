f543a15b2975059610b41c30e21848de0734d90f
AndreyKoltsov1997 <a99920641@gmail.com>
2025-11-11 17:40:02 +0100

    Update version referenced within *.sh / *.cmd scripts

--- context/generated/teamcity-minimal-agent-2025.11-linux.cmd
+++ context/generated/teamcity-minimal-agent-2025.11-linux.cmd
@@ -1,6 +1,6 @@
 cd ../..
-docker pull ubuntu:22.04
+docker pull ubuntu:24.04
 echo TeamCity/webapps > context/.dockerignore
 echo TeamCity/devPackage >> context/.dockerignore
 echo TeamCity/lib >> context/.dockerignore
-docker build -f "context/generated/linux/MinimalAgent/Ubuntu/22.04/Dockerfile" -t teamcity-minimal-agent:2025.11-linux "context"
+docker build -f "context/generated/linux/MinimalAgent/Ubuntu/24.04/Dockerfile" -t teamcity-minimal-agent:2025.11-linux "context"
