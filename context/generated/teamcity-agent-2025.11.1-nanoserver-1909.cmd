3ec4700be0f6e21dc2b7c14fbcd1c3172763e5ab
Daria Krupkina <daria.krupkina@jetbrains.com>
2025-12-15 11:43:14 +0100

    TeamCity 2025.11.1

---
+++ context/generated/teamcity-agent-2025.11.1-nanoserver-1909.cmd
@@ -0,0 +1,10 @@
+cd ../..
+docker pull mcr.microsoft.com/windows/nanoserver:1909
+docker pull mcr.microsoft.com/powershell:nanoserver-1909
+docker pull mcr.microsoft.com/dotnet/framework/sdk:4.8-windowsservercore-1909
+echo TeamCity/webapps > context/.dockerignore
+echo TeamCity/devPackage >> context/.dockerignore
+echo TeamCity/lib >> context/.dockerignore
+docker build -f "context/generated/windows/MinimalAgent/nanoserver/1909/Dockerfile" -t teamcity-minimal-agent:2025.11.1-nanoserver-1909 "context"
+docker build -f "context/generated/windows/Agent/windowsservercore/1909/Dockerfile" -t teamcity-agent:2025.11.1-windowsservercore-1909 "context"
+docker build -f "context/generated/windows/Agent/nanoserver/1909/Dockerfile" -t teamcity-agent:2025.11.1-nanoserver-1909 "context"
